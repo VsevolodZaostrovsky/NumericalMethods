5c9ad2feca0f1e9a72564e78eb8c41eae8a43ad7
VsevolodZaostrovsky <zaos2013@mail.ru>
2024-04-12 23:01:21 +0300

    dont work for different dimensions

--- PDE/Code/src/PDE.c
+++ PDE/Code/src/PDE.c
@@ -4,9 +4,7 @@ double func(double t, double x){ return 0; }
 
 double p(double x){ return 0; }
 
-double u0(double x){ return 0; }
-
-double u1(double x){ return 0; }
+double u0(double x){ return sin(M_PI * x); }
 
 
 int main(int argc, char *argv[])
@@ -36,47 +34,70 @@ int main(int argc, char *argv[])
     M = atoi(argv[2]);
 
 
-    h = (double)(1 / N);
-    tau = (double)(1 / M);
+    h   = 1. / (double)(N);
+    tau = 1. / (double)(M);
 
-    a     = (double *)malloc((N + 1) * sizeof(double));
-    b     = (double *)malloc((N + 1) * sizeof(double));
-    c     = (double *)malloc((N + 1) * sizeof(double));
-    f     = (double *)malloc((N + 1) * sizeof(double));
-    y     = (double *)malloc((N + 1) * sizeof(double));
-    alpha = (double *)malloc((N + 1) * sizeof(double));
-    beta  = (double *)malloc((N + 1) * sizeof(double));
-    differentialNet = (double *)malloc((N + 1) * (M + 1) * sizeof(double));
-    for(int k = 0; k < M + 1; k++){ differentialNet[k] = 0; }
+    a     = (double *)malloc((M + N + 1) * sizeof(double));
+    b     = (double *)malloc((M + N + 1) * sizeof(double));
+    c     = (double *)malloc((M + N + 1) * sizeof(double));
+    f     = (double *)malloc((M + N + 1) * sizeof(double));
+    y     = (double *)malloc((M + N + 1) * sizeof(double));
+    alpha = (double *)malloc((M + N + 1) * sizeof(double));
+    beta  = (double *)malloc((M + N + 1) * sizeof(double));
+    differentialNet = (double *)malloc(N * M * sizeof(double));
+    for(int k = 0; k < M; k++){ differentialNet[k] = u0(k * h); }
 
-    for(int i = 0; i < N - 1; i++)
+    for(int i = 0; i < N - 2; i++)
     {
-        for(int k = 0; k < M - 1; k++)
+        for(int k = 0; k < M - 2; k++)
         {
             a[k] = 1 / (h * h);
             b[k] = 1 / (h * h);
-            c[k] = 1 / tau + 2 / (h * h) + p(i * h);
+            c[k] = 1 / tau + 2 / (h * h) + p((i + 1) * h);
         }
 
 
-        for(int k = 0; k < M - 1; k++)
+        for(int k = 0; k < M - 2; k++)
         {
-            f[k] = func((k+1) * tau, i * h) + differentialNet[(M - 1) * i + k + 1];
-
+            f[k] = func((k+2) * tau, i * h) + differentialNet[M * i + k + 1] / tau;
+            //  printf("%lf \n", f[k]);
         }
 
-        sweep_method(a, b, c, f, y, alpha, beta, N - 2);
-        for(int k = 0; k < M - 1; k++){ differentialNet[(M - 1) * (i + 1) + k + 1] = y[k]; }
-        differentialNet[(M - 1) * (i + 1) + 0] = u0(i * h);
-        differentialNet[(M - 1) * (i + 1) + N] = u1(i * h);
+        for(int k = 0; k < M - 2; k++)
+        {
+            // printf("%lf %lf %lf %lf\n", a[k], b[k], c[k], f[k]);
+        }
+
+
+        sweep_method(a, b, c, f, y, alpha, beta, M - 3);
+        for(int k = 0; k < M - 2; k++)
+        {
+            // printf("%lf \n", y[k]);
+        }
+        for(int k = 0; k < M - 2; k++){ 
+            differentialNet[M * (i + 1) + k + 1] = y[k]; 
+        // printf("%lf ", y[k]);
+        }
+        differentialNet[M * (i + 1) + 0] = 0;
+        differentialNet[M * (i + 1) + N - 1] = 0;
     }
     
 
-    for(int i = 0; i < N + 1; i++)
-        for(int j = 0; j < M + 1; j++)
-            printf("%10.15lf ", differentialNet[j]);
-        print("\n");
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+            printf("%10.5lf ", differentialNet[i * N + j]);
+        printf("\n");
+    }
     printf("\n");
+    // for(int i = 0; i < N; i++)
+    // {
+    //     for(int j = 0; j < M; j++)
+    //         printf("%10.5lf ", sin(M_PI * j * h) * exp(-M_PI * M_PI * i * tau));
+    //     printf("\n");
+    // }
+    // printf("\n");
+
 
     free(a);
     free(b);
--- PDE/Code/src/SweepMethod.h
+++ PDE/Code/src/SweepMethod.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/* На самом деле, система должна иметь размер N+1, здесь я следую обозначеням Чижонкова Бахвалова и Корнева*/
+/* На самом деле, система должна иметь размер N+1, здесь я следую обозначениям Чижонкова Бахвалова и Корнева*/
 int sweep_method(double * a, double * b, double * c, double * f, double * y, double * alpha, double * beta, int N);

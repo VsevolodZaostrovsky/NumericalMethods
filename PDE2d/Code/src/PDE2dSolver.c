c0f6f46242ddfaf33e1a1de09e8693201f9f2613
VsevolodZaostrovsky <zaos2013@mail.ru>
2024-05-09 11:40:10 +0300

    it works but i am going try to pack it

--- PDE2d/Code/src/PDE2dSolver.c
+++ PDE2d/Code/src/PDE2dSolver.c
@@ -1,21 +1,21 @@
 #include "PDE2dSolver.h"
 
 
-void Dfromf(double * LastLayer,  double (*f)(double, double, double), double h, double tau, int G,
+void Dfromf(double (*f)(double, double, double), double h, double tau, int G,
                            double *Umatrix, double *Dmatrix, double *Cmatrix,
                            double *fmemory, double *netmemory, double *umemory, double *phimemory){
     
 
     
-    int N = (int)(1. / (h)) + 1;
+    int N_x = (int)(1. / (h)) + 1;
 
     // GenerateNet(net, N - 1);
 
-    for (int i = 1; i < N + 1; ++i)
+    for (int i = 1; i < N_x + 1; ++i)
     {
-        for (int j = 1; j < N + 1; ++j)
+        for (int j = 1; j < N_x + 1; ++j)
         {
-            Umatrix[e(i, j, N)] = LastLayer[e(i, j, N)] / tau + f(tau * (double)(G), h * (double)(i-1) , h * (double)(j-1));
+            Umatrix[e(i, j, N_x)] = f(tau * (double)(G), h * (double)(i-1) , h * (double)(j-1));
         }
     }
     // printf("U:\n");
@@ -29,49 +29,32 @@ void Dfromf(double * LastLayer,  double (*f)(double, double, double), double h, 
     // }
     // printf("\n");
 
-    FullDMatrix(Dmatrix, Umatrix, N, umemory, phimemory);
-    FullCMatrix(Dmatrix, Cmatrix, N, fmemory, umemory, phimemory);
+    FullDMatrix(Dmatrix, Umatrix, N_x, umemory, phimemory);
+    FullCMatrix(Dmatrix, Cmatrix, N_x, fmemory, umemory, phimemory);
 }
 
-void CFromD(double * C, double * D, double h, double tau) 
-{
-    int N = (int)(1. / (h)) + 1;
-    double sinPih_n = 0;
-    double sinPih_m = 0;
-
-    for (int n = 1; n < N + 1; ++n)
-    {
-        for (int m = 1; m < N + 1; ++m)
-        {
-            if (n == 1 || n == N || m == 1 || m == N)
-                C[e(n, m, N)] = 0;
-            else {
-                sinPih_n = sin(M_PI * (double)(n-1) * h * 0.5);
-                sinPih_m = sin(M_PI * (double)(m-1) * h * 0.5);
-                // C[e(n, m, N)] = D[e(n, m, N)] / (4 * (sinPih_n * sinPih_n + sinPih_m * sinPih_m) / (h * h) + 1. / tau);
-                C[e(n, m, N)] = D[e(n, m, N)] / (4 / (h * h) * (sinPih_n * sinPih_n + sinPih_m * sinPih_m) + 1. / tau );
-            }
-        }
-    }
-}
-
-void CalculateFourierOnNet(double * ans, double * C, double h)
+void CFromDandLayer(double * C, double * D, double * LastLayer, double h, double tau) 
 {
     int N_x = (int)(1. / (h)) + 1;
+    double sinPih_n = 0;
+    double sinPih_m = 0;
 
     for (int n = 1; n < N_x + 1; ++n)
     {
         for (int m = 1; m < N_x + 1; ++m)
         {
             if (n == 1 || n == N_x || m == 1 || m == N_x)
-                ans[e(n, m, N_x)] = 0;
+                C[e(n, m, N_x)] = 0;
             else {
-                ans[e(n, m, N_x)] = Calc2DFourier(C, N_x, (double)(n-1) * h, (double)(m-1) * h);
+                sinPih_n = sin(M_PI * (double)(n-1) * h * 0.5);
+                sinPih_m = sin(M_PI * (double)(m-1) * h * 0.5);
+                // C[e(n, m, N)] = D[e(n, m, N)] / (4 * (sinPih_n * sinPih_n + sinPih_m * sinPih_m) / (h * h) + 1. / tau);
+                C[e(n, m, N_x)] = (D[e(n, m, N_x)] + LastLayer[e(n, m, N_x)] / tau) / (4 / (h * h) * (sinPih_n * sinPih_n + sinPih_m * sinPih_m) + 1. / tau );
             }
         }
     }
-
 }
+
 
 double u0(double x, double y)
 {
@@ -111,6 +94,32 @@ void printMatrix(double * matrix, int N){
         printf("\n");
 }
 
+void solvePDE2d(double (*u0)(double, double),
+                double (*f)(double, double, double), 
+                int N_x, int N,
+                // все массивы ниже - память, необходимая программе 
+                double * uijn, 
+                double *Umatrix, double *Dmatrix, double *Cmatrix,
+                double *net, double *fmemory, double *netmemory, double *umemory, double *phimemory)
+                {
+    double h = 1./(double)(N_x-1);
+    double tau = 1./(double)N;
+    
+    FindFourierCoefs(Umatrix, Dmatrix, uijn, N_x, fmemory, net, u0, netmemory, umemory, phimemory);
+    
+    
+    for (int G = 1; G < N; G++)
+    {
+        // printf("D from f: \n");
+        Dfromf(f, h, tau, G, Umatrix, Dmatrix, Cmatrix, fmemory, netmemory, umemory, phimemory);
+        // printMatrix(Cmatrix, N_x);
+        // printf("Answer %d layer: \n", G);
+        CFromDandLayer(uijn + G * N_x * N_x, Cmatrix, uijn + (G - 1) * N_x * N_x, h, tau);
+        // printMatrix(uijn + G * N_x * N_x, N_x);
+
+                }
+        }
+
 int main(void){
     double x = 0;
     double y = 0;
@@ -135,42 +144,50 @@ int main(void){
     double *uijn;
 
 
-    Umatrix   = (double *)malloc(N * N * sizeof(double));
-    Dmatrix   = (double *)malloc(N * N * sizeof(double));
-    Cmatrix   = (double *)malloc(N * N * sizeof(double));
-    LastLayer = (double *)malloc(N * N * sizeof(double));
-    uijn      = (double *)malloc(N * N * N * sizeof(double));
+    Umatrix   = (double *)malloc(N_x * N_x * sizeof(double));
+    Dmatrix   = (double *)malloc(N_x * N_x * sizeof(double));
+    Cmatrix   = (double *)malloc(N_x * N_x * sizeof(double));
+    LastLayer = (double *)malloc(N_x * N_x * sizeof(double));
+    uijn      = (double *)malloc(N_x * N_x * N * sizeof(double));
 
-    for(int i = 0; i < N * N; i++){
+    for(int i = 0; i < N_x * N_x; i++){
         Umatrix[i] = 0; 
         Dmatrix[i] = 0; 
         Cmatrix[i] = 0; 
         LastLayer[i] = 0;
     }
-    for(int i = 0; i < N * N * N_x; i++)
+    for(int i = 0; i < N * N_x * N_x; i++)
         uijn[i] = 0;
     
 
-    netmemory = (double *)malloc((N + 5) * sizeof(double));
-    fmemory = (double *)malloc((N + 5) * sizeof(double));
-    net = (double *)malloc((N + 5) * sizeof(double));
-    umemory = (double *)malloc((N + 5) * sizeof(double));
-    phimemory = (double *)malloc((N + 5) * sizeof(double));
+    netmemory = (double *)malloc((N_x + 5) * sizeof(double));
+    fmemory   = (double *)malloc((N_x + 5) * sizeof(double));
+    net       = (double *)malloc((N_x + 5) * sizeof(double));
+    umemory   = (double *)malloc((N_x + 5) * sizeof(double));
+    phimemory = (double *)malloc((N_x + 5) * sizeof(double));
 
-    FullZeroLayer(uijn, u0, h);
+    FindFourierCoefs(Umatrix, Dmatrix, uijn, N_x, fmemory, net, u0, netmemory, umemory, phimemory);
+    // printMatrix(uijn, N_x);
     for (int G = 1; G < N; G++)
     {
         // printf("D from f: \n");
-        Dfromf(uijn + (G - 1) * N_x * N_x,  f, h, tau, G, Umatrix, Dmatrix, Cmatrix, fmemory, netmemory, umemory, phimemory);
+        Dfromf(f, h, tau, G, Umatrix, Dmatrix, Cmatrix, fmemory, netmemory, umemory, phimemory);
         // printMatrix(Cmatrix, N_x);
-        printf("C from D: \n");
-        CFromD(Dmatrix, Cmatrix, h, tau);
-        printMatrix(Dmatrix, N_x);
+        // printf("Answer %d layer: \n", G);
+        // CFromD(Dmatrix, Cmatrix, h, tau);
+        CFromDandLayer(uijn + G * N_x * N_x, Cmatrix, uijn + (G - 1) * N_x * N_x, h, tau);
+        // printMatrix(uijn + G * N_x * N_x, N_x);
         // printf("Final calc - u: \n");
-        CalculateFourierOnNet(uijn + G * N_x * N_x, Dmatrix, h);
+        // CalculateFourierOnNet(uijn + G * N_x * N_x, Dmatrix, h);
         // printMatrix(uijn + G * N_x * N_x, N_x);
 
     }
+
+    for(int j = 0; j < N; j++)
+    {
+        printf("%lf ", uijn[e(2, 2, N_x) + j * N_x * N_x]);
+    }
+    printf("\n");
 
     // for(int n = 1; n < 2; n++){
     //     for(int i = 1; i < N_x + 1; i++){

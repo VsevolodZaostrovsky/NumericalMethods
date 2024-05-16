d895c1330dec5b552d68220e77ee07826d800977
VsevolodZaostrovsky <zaos2013@mail.ru>
2024-05-16 18:42:23 +0300

    now it should work correctly on bgch pc

--- PDE2d/Code/src/2dFourierApprox.h
+++ PDE2d/Code/src/2dFourierApprox.h
@@ -7,7 +7,17 @@
 #define e(i, j, n) ((i - 1) * (n) + (j - 1))
 // #define e(i, j, n, N_x, N) ((n - 1) * (n * n) + (j - 1))
 
+double FourierCompute(double *coefs, int N, double x);
+
+// требует массив длины N+1, записывает в него сеть узлов, сгенерированных по правилам, описанным в репорте
 int GenerateNet(double *net, int N);
+
+// функции, вычисляющие и записывающие базисный вектор
+double Phi(int n, int i, int N);
+
+void WritePhiTo(int m, int N, double *ph);
+
+int WriteCNkTo(int N, double *cNks, double *u, double *umemory, double *phimemory);
 
 void FullUMatrix(double *Umatrix, int N, double *net, double (*u)(double, double));
 
@@ -15,20 +25,13 @@ void FullDMatrix(double *Dmatrix, double *Umatrix, int N, double *umemory, doubl
 
 void FullCMatrix(double *Dmatrix, double *Cmatrix, int N, double *fmemory, double *umemory, double *phimemory);
 
-double Calc2DFourier(double *Cmatrix, int N, double x, double y);
-
-double FourierCompute(double * coefs, int N, double x);
-
-double Calc2DFourier(double *Cmatrix, int N, double x, double y);
-
-double FullFurierCompute(double x, double y,
-        double *Umatrix, double *Dmatrix, double *Cmatrix, int N,
-            double *fmemory, double *net, double (*u)(double, double),
-                double *netmemory, double *umemory, double *phimemory);
-
 void FindFourierCoefs(double *Umatrix, double *Dmatrix, double *Cmatrix, int N,
                       double *fmemory, double *net, double (*u)(double, double),
                       double *netmemory, double *umemory, double *phimemory);
 
+double Calc2DFourier(double *Cmatrix, int N, double x, double y);
 
-#define e(i, j, n) ((i - 1) * (n) + (j - 1))
+double FullFurierCompute(double x, double y,
+                         double *Umatrix, double *Dmatrix, double *Cmatrix, int N,
+                         double *fmemory, double *net, double (*u)(double, double),
+                         double *netmemory, double *umemory, double *phimemory);
